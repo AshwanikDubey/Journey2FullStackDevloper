0ab568c5dcefd4f4697f04180821095f8dca0ba2
AshwanikDubey <ashwanidubey.work@gmail.com>
2024-12-02 22:46:54 +0530

    Build a service to get all the product data

--- src/main/java/com/example/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/example/productservice/services/FakeStoreProductService.java
@@ -6,28 +6,47 @@ import com.example.productservice.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Service
+import java.util.ArrayList;
+import java.util.List;
+
+@Service  // Service is batter naming in service component is avaliable and spring will provide object of service
 public class FakeStoreProductService implements ProductService{
 
-    RestTemplate restTemplate ;
+    RestTemplate restTemplate ; // for calling fakestore most basic this is create a http client then i can create request than i can call.
+    // A batter way we can create RestTemplate which is provide by spring framework
 
-    public FakeStoreProductService(RestTemplate restTemplate) {
+
+    public FakeStoreProductService(RestTemplate restTemplate) { // Inject the object of rest template via constuctor
       this.restTemplate  = restTemplate;
     }
 
     @Override
     public Product getProductById(Long id) {
-
+// DTO stand for Data Transfer Object its contract between 2 services when i call your Api i will get data in this form
         FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products" + id,
                 FakeStoreProductDto.class); // Here we convert JSON to Object and its called  Descrilization
         return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
     }
+
+    @Override
+    public List<Product> getAllProducts() {
+        // call to DTO
+        FakeStoreProductDto[] fakeStoreProductDtoList =
+                restTemplate.getForObject("https://fakestoreapi.com/products", FakeStoreProductDto[].class); // return type is array of fakestore DTO
+        // for return list of product we need to convert dto to product list
+        List<Product> products = new ArrayList<>();
+        for (FakeStoreProductDto fakeStoreProductDto1 : fakeStoreProductDtoList) {
+            products.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto1));
+        }
+        return products;
+    }
+
     private Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
 
         if(fakeStoreProductDto == null){
             return null;
         }
-
+// here we convert fakestore DTO to product
         Product product = new Product();
         product.setId(fakeStoreProductDto.getId());
         product.setTitle(fakeStoreProductDto.getTitle());
--- src/main/java/com/example/productservice/services/ProductService.java
+++ src/main/java/com/example/productservice/services/ProductService.java
@@ -2,7 +2,11 @@ package com.example.productservice.services;
 
 import com.example.productservice.models.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
     Product getProductById(Long id);
+
+    List<Product> getAllProducts();
 }
